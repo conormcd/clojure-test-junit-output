aa184ba6e44aa2a7050caf90b7eeea31f1d21f98
Conor McDermottroe <conor@mcdermottroe.com>
2016-02-15 01:11:45 +0000

    Make Clojars deployment work.

--- project.clj
+++ project.clj
@@ -4,4 +4,9 @@
   :license {:name "BSD"
             :url "https://github.com/conormcd/clojure-test-junit-output/blob/master/LICENSE"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [robert/hooke "1.3.0"]])
+                 [robert/hooke "1.3.0"]]
+  :pedantic? :abort
+  :deploy-repositories ^:replace [["clojars" {:url "https://clojars.org/repo"
+                                              :username [:gpg :env/clojars_username]
+                                              :password [:gpg :env/clojars_password]
+                                              :sign-releases false}]])
